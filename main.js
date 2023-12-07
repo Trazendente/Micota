157a473894d942df85b6e3653980e1b96c389d54
Glitch (glitch-hello-website) <none>
2023-12-07 19:18:59 +0000

    Exporting to Trazendente/chroma-funcional-v1 ./main.js:63453741/224 ./.glitch-assets:63453741/438

--- main.js
+++ main.js
@@ -15,13 +15,13 @@ document.addEventListener('DOMContentLoaded', () => {
     });
     const {renderer, scene, camera} = mindarThree;
 
-    const videoTexture = await loadVideo("https://cdn.glitch.global/5b7a1209-5438-4fcd-96dc-ba81f0837a93/AR_CR_PLANO_02-converted.mp4?v=1701976069202");
+    const videoTexture = await loadVideo("https://cdn.glitch.global/5b7a1209-5438-4fcd-96dc-ba81f0837a93/AR_CR_PLANO_02-converted.mp4?v=1701976517418");
     const video = videoTexture.image;
 
     const geometry = new THREE.PlaneGeometry(1, 1080/1920);
     const material = createChromaMaterial(videoTexture, 0x00ff00);
     const plane = new THREE.Mesh(geometry, material);
-    plane.rotation.x = Math.PI/2;
+    plane.rotation.x = 0;
     plane.position.y = 0.7;
     plane.scale.multiplyScalar(4);
 
