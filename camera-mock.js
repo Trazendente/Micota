1b459676842448b80503b8570917d5ef901b9f59
Glitch (glitch-hello-website) <none>
2023-12-07 16:43:24 +0000

    🗾🐫 Checkpoint ./camera-mock.js:63453741/15081

--- camera-mock.js
+++ camera-mock.js
@@ -1,43 +1,38 @@
 export const mockWithVideo = (path) => {
-  navigator.mediaDevices.getUserMedia = () => {
-    return new Promise((resolve, reject) => {
+  navigator.mediaDevices.getUserMedia = async () => {
+    return new Promise(async (resolve, reject) => {
       const video = document.createElement("video");
-
-      video.oncanplay = () => {
-	const startButton = document.createElement("button");
-	startButton.innerHTML = "start";
-	startButton.style.position = 'fixed';
-	startButton.style.zIndex = 10000;
-	document.body.appendChild(startButton);
-
-	startButton.addEventListener('click', () => {
-	  const stream = video.captureStream();
-	  video.play();
-	  document.body.removeChild(startButton);
-	  resolve(stream);
-	});
-      };
       video.setAttribute('loop', '');
       video.setAttribute("src", path);
+
+      const startButton = document.createElement("button");
+      startButton.innerHTML = "Start";
+      startButton.style.position = 'fixed';
+      startButton.style.zIndex = 10000;
+      document.body.appendChild(startButton);
+
+      const playVideo = async () => {
+        try {
+          const stream = video.captureStream();
+          await video.play();
+          document.body.removeChild(startButton);
+          resolve(stream);
+        } catch (error) {
+          reject(error);
+        }
+      };
+
+      // Agregar evento de clic al botón para iniciar la reproducción
+      startButton.addEventListener('click', playVideo);
+
+      // Opcional: Agregar evento de clic en cualquier lugar de la página para iniciar la reproducción
+      document.body.addEventListener('click', playVideo);
+
+      // Opcional: Pausar el video al principio
+      video.pause();
+
+      // Cargar el video después de la interacción del usuario
+      await video.load();
     });
   };
-}
-
-export const mockWithImage = (path) => {
-  navigator.mediaDevices.getUserMedia = () => {
-    return new Promise((resolve, reject) => {
-      const canvas = document.createElement("canvas");
-      const context = canvas.getContext('2d');
-
-      const image = new Image();
-      image.onload = () => {
-	canvas.width = image.width;
-	canvas.height = image.height;
-	context.drawImage(image, 0, 0, image.width, image.height);
-	const stream = canvas.captureStream();
-	resolve(stream);
-      }
-      image.src = path;
-    });
-  };
-}
+};
