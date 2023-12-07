3cd56e90ed3c4a216d9fb7f956ed98240ebea0e3
Glitch (croma-funcional) <none>
2023-12-07 20:24:29 +0000

    👨🐇 Checkpoint ./chroma-video.js:63453741/11025

--- chroma-video.js
+++ chroma-video.js
@@ -1,6 +1,6 @@
 import * as THREE from "./three.module.js";
 
-export const createChromaMaterial = (texture, keyColor) => {
+export const createChromaMaterial = (texture, keyColor, tolerance = 0.2) => {
   const keyColorObject = new THREE.Color(keyColor);
   const material = new THREE.ShaderMaterial({
     uniforms: {
@@ -11,26 +11,32 @@ export const createChromaMaterial = (texture, keyColor) => {
       color: {
         type: "c",
         value: keyColorObject
+      },
+      tolerance: {
+        type: "f",
+        value: tolerance
       }
     },
     vertexShader:
-    "varying mediump vec2 vUv;\n" +
-    "void main(void)\n" +
-    "{\n" +
-    "vUv = uv;\n" +
-    "mediump vec4 mvPosition = modelViewMatrix * vec4( position, 1.0 );\n" +
-    "gl_Position = projectionMatrix * mvPosition;\n" +
-    "}",
+      "varying mediump vec2 vUv;\n" +
+      "void main(void)\n" +
+      "{\n" +
+      "vUv = uv;\n" +
+      "mediump vec4 mvPosition = modelViewMatrix * vec4( position, 1.0 );\n" +
+      "gl_Position = projectionMatrix * mvPosition;\n" +
+      "}",
     fragmentShader:
-    "uniform mediump sampler2D tex;\n" +
-    "uniform mediump vec3 color;\n" +
-    "varying mediump vec2 vUv;\n" +
-    "void main(void)\n" +
-    "{\n" +
-    "  mediump vec3 tColor = texture2D( tex, vUv ).rgb;\n" +
-    "  mediump float a = (length(tColor - color) - 0.7) * 7.0;\n" +
-    "  gl_FragColor = vec4(tColor, a);\n" +
-    "}",
+      "uniform mediump sampler2D tex;\n" +
+      "uniform mediump vec3 color;\n" +
+      "uniform float tolerance;\n" +
+      "varying mediump vec2 vUv;\n" +
+      "void main(void)\n" +
+      "{\n" +
+      "  mediump vec3 tColor = texture2D( tex, vUv ).rgb;\n" +
+      "  mediump float d = length(tColor - color);\n" +
+      "  mediump float a = smoothstep(tolerance, tolerance + 0.1, d);\n" +
+      "  gl_FragColor = vec4(tColor, a);\n" +
+      "}",
     transparent: true
   });
   return material;
