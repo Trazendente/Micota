babb6c569caec633870e318983cff19713a91526
Glitch (croma-funcional) <none>
2023-12-11 15:14:22 +0000

    🐲👡 Checkpoint ./main.js:63453741/26320 ./loader.js:63453741/242451

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 // main.js
 import {loadVideo} from "./loader.js";
-import {loadAudio} from "./loader.js";
 import {createChromaMaterial} from './chroma-video.js';
 
 const THREE = window.MINDAR.IMAGE.THREE;
@@ -63,7 +62,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
       return { video, plane };
     }));
-    
+
     const audioClip = await loadAudio('https://cdn.glitch.global/5b7a1209-5438-4fcd-96dc-ba81f0837a93/AUDIO_CR_V1_2.mp3?v=1702306241238');
 
     const listener = new THREE.AudioListener();
@@ -82,7 +81,8 @@ document.addEventListener('DOMContentLoaded', () => {
     anchor.onTargetLost = () => {
       audio.pause();
     }
-
+    
+    
     await mindarThree.start();
 
     renderer.setAnimationLoop(() => {
