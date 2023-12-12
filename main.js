72a023e81aa3425331fdc7dc0e1912bf7e7b4b78
Glitch (croma-funcional) <none>
2023-12-12 16:46:47 +0000

    🦂⛈ Checkpoint ./.glitch-assets:63453741/401 ./main.js:63453741/950

--- main.js
+++ main.js
@@ -60,7 +60,7 @@ document.addEventListener("DOMContentLoaded", () => {
         position: new THREE.Vector3(0, 0, 0.5),
       },
       {
-        url: "https://cdn.glitch.global/5b7a1209-5438-4fcd-96dc-ba81f0837a93/Piso-V1-MAIN.mp4?v=1702396063612",
+        url: "https://cdn.glitch.global/5b7a1209-5438-4fcd-96dc-ba81f0837a93/Piso-V1-MAIN.mp4?v=1702399320694",
         position: new THREE.Vector3(0, -0.25, 0.35),
     rotation: new THREE.Euler(-Math.PI / 2, 0, Math.PI), // Rotación de 90 grados en el eje Y
   scale: new THREE.Vector3(0.5, 0.7, 0.5), // Agregar escala
