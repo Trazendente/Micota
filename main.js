0dbe28ca604bbba651f949c782cf0fd37d24d45a
Glitch (croma-funcional) <none>
2023-12-12 15:47:43 +0000

    🎡🔦 Checkpoint ./main.js:63453741/66 ./index.html:63453741/1102

--- main.js
+++ main.js
@@ -10,8 +10,6 @@ document.addEventListener("DOMContentLoaded", () => {
       container: document.body,
       imageTargetSrc:
         "https://cdn.glitch.global/5b7a1209-5438-4fcd-96dc-ba81f0837a93/target-cr.mind?v=1701976017267",
-      uiScanning:
-      uiLoading: "no"
     
     });
 
