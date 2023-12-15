6c49a3056395656ed60d74c9b8c828f030afeef9
Glitch (croma-funcional) <none>
2023-12-15 18:30:47 +0000

    🏃🐀 Checkpoint ./main.js:63453741/137

--- main.js
+++ main.js
@@ -21,10 +21,7 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 
     const { renderer, scene, camera } = mindarThree;
-    // Configuración de la cámara
-  camera.near = 0.01; // Ajusta según sea necesario
-  camera.far = 5000; // Ajusta según sea necesario
-
+    
 
     // Configuración del audio
     const audioClipPromise = loadAudio(
@@ -73,7 +70,7 @@ document.addEventListener("DOMContentLoaded", () => {
       },
       {
         url: "https://cdn.glitch.global/5b7a1209-5438-4fcd-96dc-ba81f0837a93/Piso-V1-MAINv2.mp4?v=1702499962115",
-        position: new THREE.Vector3(0, -0.25, 0.3),
+        position: new THREE.Vector3(0, -0.26, 0.3),
         rotation: new THREE.Euler(-Math.PI / 2, 0, Math.PI), // Rotación de 90 grados en el eje Y
         scale: new THREE.Vector3(0.5, 0.7, 0.5), // Agregar escala
       },
