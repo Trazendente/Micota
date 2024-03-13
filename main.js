975bf1fe8760299ee7d5d161bd82913b099f3ddf
Glitch (croma-funcional) <none>
2024-03-13 20:40:47 +0000

    ✌️👏 Checkpoint ./main.js:63453741/9457 ./.glitch-assets:63453741/359

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const THREE = window.MINDAR.IMAGE.THREE;
 
 document.addEventListener("DOMContentLoaded", () => {
   let experienceStarted = false;
-  let mixer1, action1, mixer2, action2; // Definimos las variables fuera de la función start para que estén accesibles en toda la función
+  let mixer1, action1, mixer2, action2;
 
   const start = async () => {
     if (experienceStarted) {
@@ -22,17 +22,13 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const { renderer, cssRenderer, scene, cssScene, camera } = mindarThree;
 
-    // Remover la luz direccional
-    scene.remove(scene.getObjectByName("directionalLight"));
-
-    // Cambiar la configuración de la luz ambiental
-    const ambientLight = new THREE.AmbientLight(0xffffff, 0.8); // Luz ambiental blanca con intensidad aumentada
-    scene.add(ambientLight);
-
-    // Agregar una luz direccional suave para iluminar la escena desde una dirección específica
-    const directionalLight = new THREE.DirectionalLight(0xffffff, 0.7); // Luz blanca con intensidad reducida
-    directionalLight.position.set(0, 1, 1); // Posición de la luz
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+    directionalLight.position.set(100, 200, 100);
+    directionalLight.castShadow = false;
     scene.add(directionalLight);
+
+    const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+    scene.add(ambientLight);
 
     camera.near = 0.01;
     camera.far = 5000;
@@ -51,7 +47,7 @@ document.addEventListener("DOMContentLoaded", () => {
     Portada1Model.scene.position.set(0, -0.5, 0);
 
     const Portada2Model = await loadGLTF(
-      "https://cdn.glitch.global/b24066b4-44c1-4e97-82b5-a492cc7e9f6f/2MARGARITA_anim_V6.glb?v=1710348736272"
+      "https://cdn.glitch.global/b24066b4-44c1-4e97-82b5-a492cc7e9f6f/5MOJITO_anim_V2.glb?v=1710361609255"
     );
 
     Portada2Model.scene.scale.set(0.3, 0.3, 0.3);
@@ -63,25 +59,8 @@ document.addEventListener("DOMContentLoaded", () => {
     Portada1Anchor.group.add(Portada1Model.scene);
     Portada2Anchor.group.add(Portada2Model.scene);
 
-    Portada1Anchor.onTargetFound = () => {
-      // Acciones al encontrar el primer objetivo
-    };
-
-    Portada2Anchor.onTargetFound = () => {
-      // Acciones al encontrar el segundo objetivo
-    };
-
-    Portada1Anchor.onTargetLost = () => {
-      // Acciones al perder el primer objetivo
-    };
-
-    Portada2Anchor.onTargetLost = () => {
-      // Acciones al perder el segundo objetivo
-    };
-
     await mindarThree.start();
 
-    // Crear mezcladores y reproducir acciones dentro de la función start
     mixer1 = new THREE.AnimationMixer(Portada1Model.scene);
     action1 = mixer1.clipAction(Portada1Model.animations[0]);
     action1.play();
@@ -99,27 +78,23 @@ document.addEventListener("DOMContentLoaded", () => {
       cssRenderer.render(cssScene, camera);
     });
   };
-  
+
+  // Crea el botón "COMENZAR"
   const startButton = document.createElement("button");
   startButton.textContent = "COMENZAR";
   startButton.id = "startButton";
-  startButton.classList.add("circle-button"); // Agregar una clase para aplicar estilos CSS
+  startButton.classList.add("circle-button");
   startButton.style.backgroundColor = "#a62424";
   startButton.style.color = "#FFFFFF";
   startButton.style.fontFamily = "Segoe, sans-serif";
 
-  startButton.addEventListener("click", start);
-  document.body.appendChild(startButton);
-  
-  // Añadir event listener al botón "COMENZAR" después de que se haya creado
-  document.getElementById("startButton").addEventListener("click", () => {
+  // Añade el evento de clic al botón "COMENZAR"
+  startButton.addEventListener("click", () => {
     document.getElementById("backgroundAudio").play();
     start();
   });
 
-  const infoText = document.createElement("p");
-  infoText.id = "infoText";
-  infoText.style.fontFamily = "Segoe, sans-serif";
+  // Agrega el botón al documento
+  document.body.appendChild(startButton);
 
-  document.body.appendChild(infoText);
 });
