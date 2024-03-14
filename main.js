0d535133236429160463a4d205c48f377f7b6ff7
Glitch (croma-funcional) <none>
2024-03-14 19:07:07 +0000

    🍛🌥 Checkpoint ./main.js:63453741/14464 ./.glitch-assets:63453741/355

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const THREE = window.MINDAR.IMAGE.THREE;
 
 document.addEventListener("DOMContentLoaded", () => {
   let experienceStarted = false;
-  let mixer1, action1, mixer2, action2, mixer3, action3, mixer4, action4, mixer5, action5;
+  let mixer1, action1, mixer2, action2, mixer3, action3, mixer4, action4, mixer5, action5, mixer6, action6, mixer7, action7, mixer8, action8;
 
   const start = async () => {
     if (experienceStarted) {
@@ -16,14 +16,14 @@ document.addEventListener("DOMContentLoaded", () => {
     const mindarThree = new window.MINDAR.IMAGE.MindARThree({
       container: document.body,
       imageTargetSrc:
-        "https://cdn.glitch.global/b24066b4-44c1-4e97-82b5-a492cc7e9f6f/targets%20(2).mind?v=1710437825729",
+        "https://cdn.glitch.global/b24066b4-44c1-4e97-82b5-a492cc7e9f6f/targets%20(4).mind?v=1710442733839",
       uiScanning: "#scanning",
       uiLoading: "no",
     });
 
     const { renderer, cssRenderer, scene, cssScene, camera } = mindarThree;
 
-    var ambientLight = new THREE.AmbientLight(0xffffff, 0.8);
+    var ambientLight = new THREE.AmbientLight(0xcccccc, 0.8);
     scene.add(ambientLight);
 
     var pointLight = new THREE.PointLight(0xffffff, 0.4);
@@ -31,6 +31,9 @@ document.addEventListener("DOMContentLoaded", () => {
     pointLight.position.y = 10;
     pointLight.position.z = 10;
     scene.add(pointLight);
+
+    const ambientLight2 = new THREE.AmbientLight(0xffffff, 4); 
+    scene.add(ambientLight2);
 
     const startButton = document.getElementById("startButton");
     const infoText = document.getElementById("infoText");
@@ -51,7 +54,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     Portada2Model.scene.scale.set(0.3, 0.3, 0.3);
     Portada2Model.scene.position.set(0, -0.5, 0);
-   
+    Portada2Model.scene.rotation.x = Math.PI;
 
     const Model3 = await loadGLTF(
       "https://cdn.glitch.global/b24066b4-44c1-4e97-82b5-a492cc7e9f6f/3RONCOLA_anim_V6.glb?v=1710356395844"
@@ -71,17 +74,41 @@ document.addEventListener("DOMContentLoaded", () => {
     Model5.scene.scale.set(0.3, 0.3, 0.3);
     Model5.scene.position.set(0, -0.5, 0);
 
+    const Model6 = await loadGLTF(
+      "https://cdn.glitch.global/b24066b4-44c1-4e97-82b5-a492cc7e9f6f/5MOJITO_anim_V2.glb?v=1710361609255"
+    );
+    Model6.scene.scale.set(0.3, 0.3, 0.3);
+    Model6.scene.position.set(0, -0.5, 0);
+
+    const Model7 = await loadGLTF(
+      "https://cdn.glitch.global/b24066b4-44c1-4e97-82b5-a492cc7e9f6f/5MOJITO_anim_V2.glb?v=1710361609255"
+    );
+    Model7.scene.scale.set(0.3, 0.3, 0.3);
+    Model7.scene.position.set(0, -0.5, 0);
+
+    const Model8 = await loadGLTF(
+      "https://cdn.glitch.global/b24066b4-44c1-4e97-82b5-a492cc7e9f6f/5MOJITO_anim_V2.glb?v=1710361609255"
+    );
+    Model8.scene.scale.set(0.3, 0.3, 0.3);
+    Model8.scene.position.set(0, -0.5, 0);
+
     const Portada1Anchor = mindarThree.addAnchor(0);
     const Portada2Anchor = mindarThree.addAnchor(1);
     const Anchor3 = mindarThree.addAnchor(2);
     const Anchor4 = mindarThree.addAnchor(3);
     const Anchor5 = mindarThree.addAnchor(4);
+    const Anchor6 = mindarThree.addAnchor(5);
+    const Anchor7 = mindarThree.addAnchor(6);
+    const Anchor8 = mindarThree.addAnchor(7);
 
     Portada1Anchor.group.add(Portada1Model.scene);
     Portada2Anchor.group.add(Portada2Model.scene);
     Anchor3.group.add(Model3.scene);
     Anchor4.group.add(Model4.scene);
     Anchor5.group.add(Model5.scene);
+    Anchor6.group.add(Model6.scene);
+    Anchor7.group.add(Model7.scene);
+    Anchor8.group.add(Model8.scene);
 
     Portada1Anchor.onTargetFound = () => {
       // Acciones al encontrar el primer objetivo
@@ -103,6 +130,18 @@ document.addEventListener("DOMContentLoaded", () => {
       // Acciones al encontrar el quinto objetivo
     };
 
+    Anchor6.onTargetFound = () => {
+      // Acciones al encontrar el sexto objetivo
+    };
+
+    Anchor7.onTargetFound = () => {
+      // Acciones al encontrar el séptimo objetivo
+    };
+
+    Anchor8.onTargetFound = () => {
+      // Acciones al encontrar el octavo objetivo
+    };
+
     Portada1Anchor.onTargetLost = () => {
       // Acciones al perder el primer objetivo
     };
@@ -121,6 +160,18 @@ document.addEventListener("DOMContentLoaded", () => {
 
     Anchor5.onTargetLost = () => {
       // Acciones al perder el quinto objetivo
+    };
+
+    Anchor6.onTargetLost = () => {
+      // Acciones al perder el sexto objetivo
+    };
+
+    Anchor7.onTargetLost = () => {
+      // Acciones al perder el séptimo objetivo
+    };
+
+    Anchor8.onTargetLost = () => {
+      // Acciones al perder el octavo objetivo
     };
 
     await mindarThree.start();
@@ -145,6 +196,18 @@ document.addEventListener("DOMContentLoaded", () => {
     action5 = mixer5.clipAction(Model5.animations[0]);
     action5.play();
 
+    mixer6 = new THREE.AnimationMixer(Model6.scene);
+    action6 = mixer6.clipAction(Model6.animations[0]);
+    action6.play();
+
+    mixer7 = new THREE.AnimationMixer(Model7.scene);
+    action7 = mixer7.clipAction(Model7.animations[0]);
+    action7.play();
+
+    mixer8 = new THREE.AnimationMixer(Model8.scene);
+    action8 = mixer8.clipAction(Model8.animations[0]);
+    action8.play();
+
     const clock = new THREE.Clock();
     renderer.setAnimationLoop(() => {
       const delta = clock.getDelta();
@@ -153,6 +216,9 @@ document.addEventListener("DOMContentLoaded", () => {
       mixer3.update(delta);
       mixer4.update(delta);
       mixer5.update(delta);
+      mixer6.update(delta);
+      mixer7.update(delta);
+      mixer8.update(delta);
       renderer.render(scene, camera);
       cssRenderer.render(cssScene, camera);
     });
