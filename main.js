c23671f66d75893af6d90e4f82f701b9d55484f7
Glitch (croma-funcional) <none>
2024-03-14 17:26:18 +0000

    🐊🎟 Checkpoint ./main.js:63453741/10704

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 import { loadGLTF, loadTextures } from "./loader.js";
 import { loadAudio } from "./loader.js";
 
@@ -6,7 +5,7 @@ const THREE = window.MINDAR.IMAGE.THREE;
 
 document.addEventListener("DOMContentLoaded", () => {
   let experienceStarted = false;
-  let mixer1, action1, mixer2, action2; // Definimos las variables fuera de la función start para que estén accesibles en toda la función
+  let mixer1, action1, mixer2, action2, mixer3, action3, mixer4, action4, mixer5, action5;
 
   const start = async () => {
     if (experienceStarted) {
@@ -24,19 +23,14 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const { renderer, cssRenderer, scene, cssScene, camera } = mindarThree;
 
-    var ambientLight = new THREE.AmbientLight(0xcccccc, 0.8);
+    var ambientLight = new THREE.AmbientLight(0xffffff, 0.8);
     scene.add(ambientLight);
 
     var pointLight = new THREE.PointLight(0xffffff, 0.4);
     pointLight.castShadow = true;
-
     pointLight.position.y = 10;
     pointLight.position.z = 10;
     scene.add(pointLight);
-
-    // Agregar luz de ambiente
-    const ambientLight2 = new THREE.AmbientLight(0xffffff, 4); // Color blanco, intensidad 1
-    scene.add(ambientLight);
 
     const startButton = document.getElementById("startButton");
     const infoText = document.getElementById("infoText");
@@ -46,25 +40,48 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // Modelos GLB
     const Portada1Model = await loadGLTF(
-      "https://cdn.glitch.global/b24066b4-44c1-4e97-82b5-a492cc7e9f6f/3RONCOLA_anim_V6.glb?v=1710356395844"
+      "https://cdn.glitch.global/b24066b4-44c1-4e97-82b5-a492cc7e9f6f/1GIN_anim_V1.glb?v=1710253081220"
     );
     Portada1Model.scene.scale.set(0.3, 0.3, 0.3);
     Portada1Model.scene.position.set(0, -0.5, 0);
 
     const Portada2Model = await loadGLTF(
-      "https://cdn.glitch.global/b24066b4-44c1-4e97-82b5-a492cc7e9f6f/5MOJITO_Anim_V5.glb?v=1710433138987"
+      "https://cdn.glitch.global/b24066b4-44c1-4e97-82b5-a492cc7e9f6f/2MARGARITA_anim_V5.glb?v=1710348085755"
     );
 
     Portada2Model.scene.scale.set(0.3, 0.3, 0.3);
     Portada2Model.scene.position.set(0, -0.5, 0);
-    Portada2Model.scene.rotation.x = Math.PI;
-    
+   
+
+    const Model3 = await loadGLTF(
+      "https://cdn.glitch.global/b24066b4-44c1-4e97-82b5-a492cc7e9f6f/3RONCOLA_anim_V6.glb?v=1710356395844"
+    );
+    Model3.scene.scale.set(0.3, 0.3, 0.3);
+    Model3.scene.position.set(0, -0.5, 0);
+
+    const Model4 = await loadGLTF(
+      "https://cdn.glitch.global/b24066b4-44c1-4e97-82b5-a492cc7e9f6f/4PALOMA_anim_V1.glb?v=1710435750120"
+    );
+    Model4.scene.scale.set(0.3, 0.3, 0.3);
+    Model4.scene.position.set(0, -0.5, 0);
+
+    const Model5 = await loadGLTF(
+      "https://cdn.glitch.global/b24066b4-44c1-4e97-82b5-a492cc7e9f6f/5MOJITO_anim_V2.glb?v=1710361609255"
+    );
+    Model5.scene.scale.set(0.3, 0.3, 0.3);
+    Model5.scene.position.set(0, -0.5, 0);
 
     const Portada1Anchor = mindarThree.addAnchor(0);
     const Portada2Anchor = mindarThree.addAnchor(1);
+    const Anchor3 = mindarThree.addAnchor(2);
+    const Anchor4 = mindarThree.addAnchor(3);
+    const Anchor5 = mindarThree.addAnchor(4);
 
     Portada1Anchor.group.add(Portada1Model.scene);
     Portada2Anchor.group.add(Portada2Model.scene);
+    Anchor3.group.add(Model3.scene);
+    Anchor4.group.add(Model4.scene);
+    Anchor5.group.add(Model5.scene);
 
     Portada1Anchor.onTargetFound = () => {
       // Acciones al encontrar el primer objetivo
@@ -72,6 +89,18 @@ document.addEventListener("DOMContentLoaded", () => {
 
     Portada2Anchor.onTargetFound = () => {
       // Acciones al encontrar el segundo objetivo
+    };
+
+    Anchor3.onTargetFound = () => {
+      // Acciones al encontrar el tercer objetivo
+    };
+
+    Anchor4.onTargetFound = () => {
+      // Acciones al encontrar el cuarto objetivo
+    };
+
+    Anchor5.onTargetFound = () => {
+      // Acciones al encontrar el quinto objetivo
     };
 
     Portada1Anchor.onTargetLost = () => {
@@ -82,9 +111,20 @@ document.addEventListener("DOMContentLoaded", () => {
       // Acciones al perder el segundo objetivo
     };
 
+    Anchor3.onTargetLost = () => {
+      // Acciones al perder el tercer objetivo
+    };
+
+    Anchor4.onTargetLost = () => {
+      // Acciones al perder el cuarto objetivo
+    };
+
+    Anchor5.onTargetLost = () => {
+      // Acciones al perder el quinto objetivo
+    };
+
     await mindarThree.start();
 
-    // Crear mezcladores y reproducir acciones dentro de la función start
     mixer1 = new THREE.AnimationMixer(Portada1Model.scene);
     action1 = mixer1.clipAction(Portada1Model.animations[0]);
     action1.play();
@@ -93,11 +133,26 @@ document.addEventListener("DOMContentLoaded", () => {
     action2 = mixer2.clipAction(Portada2Model.animations[0]);
     action2.play();
 
+    mixer3 = new THREE.AnimationMixer(Model3.scene);
+    action3 = mixer3.clipAction(Model3.animations[0]);
+    action3.play();
+
+    mixer4 = new THREE.AnimationMixer(Model4.scene);
+    action4 = mixer4.clipAction(Model4.animations[0]);
+    action4.play();
+
+    mixer5 = new THREE.AnimationMixer(Model5.scene);
+    action5 = mixer5.clipAction(Model5.animations[0]);
+    action5.play();
+
     const clock = new THREE.Clock();
     renderer.setAnimationLoop(() => {
       const delta = clock.getDelta();
       mixer1.update(delta);
       mixer2.update(delta);
+      mixer3.update(delta);
+      mixer4.update(delta);
+      mixer5.update(delta);
       renderer.render(scene, camera);
       cssRenderer.render(cssScene, camera);
     });
@@ -106,7 +161,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const startButton = document.createElement("button");
   startButton.textContent = "COMENZAR";
   startButton.id = "startButton";
-  startButton.classList.add("circle-button"); // Agregar una clase para aplicar estilos CSS
+  startButton.classList.add("circle-button");
   startButton.style.backgroundColor = "#a62424";
   startButton.style.color = "#FFFFFF";
   startButton.style.fontFamily = "Segoe, sans-serif";
@@ -114,7 +169,6 @@ document.addEventListener("DOMContentLoaded", () => {
   startButton.addEventListener("click", start);
   document.body.appendChild(startButton);
 
-  // Añadir event listener al botón "COMENZAR" después de que se haya creado
   document.getElementById("startButton").addEventListener("click", () => {
     document.getElementById("backgroundAudio").play();
     start();
